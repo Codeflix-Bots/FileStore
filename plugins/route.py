760acef2a548c1cbf67eacf83097dc22178dd97f
Codeflix-Bots <159244479+Codeflix-Bots@users.noreply.github.com>
2024-03-03 15:07:05 +0530

    Add files via upload

---
+++ plugins/route.py
@@ -0,0 +1,14 @@
+#(©)Codeflix_Bots
+#rymme
+
+
+
+
+
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("Codeflix_Bots (Telegram)")
