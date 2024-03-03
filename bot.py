570767a379375b9b53636a004770882a37a97c44
Codeflix-Bots <159244479+Codeflix-Bots@users.noreply.github.com>
2024-03-03 17:15:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -75,10 +75,8 @@ class Bot(Client):
 
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/weebs_support")
-        self.LOGGER(__name__).info(f""" \n\n       
-                                                   
-                  
-                                 
+        self.LOGGER(__name__).info(f"""       
+
 
   ___ ___  ___  ___ ___ _    _____  _____  ___ _____ ___ 
  / __/ _ \|   \| __| __| |  |_ _\ \/ / _ )/ _ \_   _/ __|
@@ -86,10 +84,6 @@ class Bot(Client):
  \___\___/|___/|___|_| |____|___/_/\_\___/\___/ |_| |___/
                                                          
  
-                                                                        
-                                                                      
-                                                                                 
-                              
                                           """)
         self.username = usr_bot_me.username
         #web-response
