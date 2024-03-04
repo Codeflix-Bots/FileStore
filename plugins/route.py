34624aa0c8781ec552b90590c911bf30a9ea1935
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-03-04 12:59:00 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -1,14 +1,7 @@
-#(©)Codexbotz
-#rymme
-
-
-
-
-
 from aiohttp import web
 
 routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("CodeXBotz")
+    return web.json_response("Codeflix FileStore")
