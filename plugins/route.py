7c5e05d798d0e493841f0444ed6d0831f821bff9
Codeflix-Bots <159244479+Codeflix-Bots@users.noreply.github.com>
2024-03-04 13:11:13 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -1,7 +1,17 @@
+# Don't Remove Credit @Codeflix-Bots
+# Subscribe Telegram Channel For Amazing Bot @Codeflix_Bots
+# Ask Doubt on telegram @sewxiy
+
 from aiohttp import web
 
 routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Codeflix FileStore")
+    return web.json_response("Codeflix_Bots")
+
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
