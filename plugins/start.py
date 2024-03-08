c875ad6525eb0f3695467cdd3cdcf352e63ca563
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-03-08 20:41:32 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -17,7 +17,7 @@ from database.database import add_user, del_user, full_userbase, present_user
 
 """add time im seconds for waitingwaiting before delete 
 1min=60, 2min=60×2=120, 5min=60×5=300"""
-SECONDS = int(os.getenv("SECONDS", "30"))
+SECONDS = int(os.getenv("SECONDS", "300"))
 
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
@@ -89,12 +89,13 @@ async def start_command(client: Client, message: Message):
                 snt_msgs.append(snt_msg)
             except:
                 pass
-        await message.reply_text("Baka! Files will be deleted After 5 minutes. Save them to the Saved Message now!")
+        SD = await message.reply_text("Baka! Files will be deleted After 5 minutes. Save them to the Saved Message now!")
         await asyncio.sleep(SECONDS)
 
         for snt_msg in snt_msgs:
             try:
                 await snt_msg.delete()
+                await SD.delete()
             except:
                 pass
         return
