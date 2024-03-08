dcd4eafbe7e41df1c8686b6b98e5657b57a3b12c
Codeflix-Bots <159244479+Codeflix-Bots@users.noreply.github.com>
2024-03-08 21:10:24 +0530

    Auto delete fixed

--- plugins/start.py
+++ plugins/start.py
@@ -17,7 +17,7 @@ from database.database import add_user, del_user, full_userbase, present_user
 
 """add time im seconds for waitingwaiting before delete 
 1min=60, 2min=60×2=120, 5min=60×5=300"""
-SECONDS = int(os.getenv("SECONDS", "300"))
+SECONDS = int(os.getenv("SECONDS", "10"))
 
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
