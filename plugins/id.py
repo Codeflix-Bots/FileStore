9d0a839697485e92a2c3a3ea9487d9f9de1a00a4
Codeflix-Bots <159244479+Codeflix-Bots@users.noreply.github.com>
2024-04-06 13:33:40 +0530

    Create id.py

---
+++ plugins/id.py
@@ -0,0 +1,20 @@
+#credit @codeflix_bots (telegram)
+
+"""Get id of the replied user
+Syntax: /id"""
+
+from pyrogram import filters, enums
+from pyrogram.types import Message
+
+from bot import Bot
+
+
+@Bot.on_message(filters.command("id") & filters.private)
+async def showid(client, message):
+    chat_type = message.chat.type
+
+    if chat_type == enums.ChatType.PRIVATE:
+        user_id = message.chat.id
+        await message.reply_text(
+            f"<b>ʏᴏᴜʀ ᴜsᴇʀ ɪᴅ ɪs:</b> <code>{user_id}</code>", quote=True
+        )
