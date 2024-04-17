c174bf6fefcaa8165aa94f7b6693202f52fd1f29
Codeflix-Bots <159244479+Codeflix-Bots@users.noreply.github.com>
2024-04-17 13:41:46 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -15,6 +15,9 @@ from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
 
+"""add time im seconds for waitingwaiting before delete 
+1min=60, 2min=60×2=120, 5min=60×5=300"""
+SECONDS = int(os.getenv("SECONDS", "10"))
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
@@ -85,15 +88,14 @@ async def start_command(client: Client, message: Message):
                 snt_msgs.append(snt_msg)
             except:
                 pass
-            SD = await message.reply_text("Baka! Files will be deleted After 30 seconds. Save them to the Saved Message now!")
-            await asyncio.sleep(30)
+        await message.reply_text("**Baka! Files will be deleted After 10 seconds. Save them to the Saved Message now!.**")
+        await asyncio.sleep(SECONDS)
 
-            for snt_msg in snt_msgs:
-                try:
-                    await snt_msg.delete()
-                    await SD.delete()
-                except:
-                    pass
+        for snt_msg in snt_msgs:
+            try:
+                await snt_msg.delete()
+            except:
+                pass
         return
     else:
         reply_markup = InlineKeyboardMarkup(
