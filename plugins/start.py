eccbe52b524a5aa153530dec3060d78c704c37c6
𝖢𝗈𝖽𝖾𝖿𝗅𝗂𝗑 𝖡𝗈𝗍𝗌 <159244479+Codeflix-Bots@users.noreply.github.com>
2024-05-30 11:17:11 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -79,7 +79,7 @@ async def start_command(client: Client, message: Message):
 
             try:
                 snt_msg = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-                await asyncio.sleep(0.5)
+                await asyncio.sleep(0.9)
                 Codeflix.append(snt_msg)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
