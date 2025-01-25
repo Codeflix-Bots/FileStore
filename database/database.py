6cd545a36f7a068f363da8162d9ccf56a23afb6e
rohit3607 <rohitplayer87089@gmail.com>
2025-01-25 19:37:16 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,36 +1,56 @@
-#(©)CodeXBotz
+#Codeflix_Botz
+#rohit_1888 on Tg
 
-
-
-
-import pymongo, os
+import motor, asyncio
+import motor.motor_asyncio
 from config import DB_URI, DB_NAME
 
-
-dbclient = pymongo.MongoClient(DB_URI)
+dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
 database = dbclient[DB_NAME]
-
 
 user_data = database['users']
 
+default_verify = {
+    'is_verified': False,
+    'verified_time': 0,
+    'verify_token': "",
+    'link': ""
+}
 
+def new_user(id):
+    return {
+        '_id': id,
+        'verify_status': {
+            'is_verified': False,
+            'verified_time': "",
+            'verify_token': "",
+            'link': ""
+        }
+    }
 
-async def present_user(user_id : int):
-    found = user_data.find_one({'_id': user_id})
+async def present_user(user_id: int):
+    found = await user_data.find_one({'_id': user_id})
     return bool(found)
 
 async def add_user(user_id: int):
-    user_data.insert_one({'_id': user_id})
+    user = new_user(user_id)
+    await user_data.insert_one(user)
     return
+
+async def db_verify_status(user_id):
+    user = await user_data.find_one({'_id': user_id})
+    if user:
+        return user.get('verify_status', default_verify)
+    return default_verify
+
+async def db_update_verify_status(user_id, verify):
+    await user_data.update_one({'_id': user_id}, {'$set': {'verify_status': verify}})
 
 async def full_userbase():
     user_docs = user_data.find()
-    user_ids = []
-    for doc in user_docs:
-        user_ids.append(doc['_id'])
-        
+    user_ids = [doc['_id'] async for doc in user_docs]
     return user_ids
 
 async def del_user(user_id: int):
-    user_data.delete_one({'_id': user_id})
+    await user_data.delete_one({'_id': user_id})
     return
