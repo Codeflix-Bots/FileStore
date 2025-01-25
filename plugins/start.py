beb075424d7f3e27d4cd42f5049e2b45b820f4e6
rohit3607 <rohitplayer87089@gmail.com>
2025-01-25 17:14:07 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -17,7 +17,7 @@ from database.database import *
 # File auto-delete time in seconds (Set your desired time in seconds here)
 FILE_AUTO_DELETE = TIME  # Example: 3600 seconds (1 hour)
 
-@Bot.on_message(filters.command('start') & filters.private & subscribed)
+@Bot.on_message(filters.command('start') & filters.private & subscribed1 & subscribed2 & subscribed3 & subscribed4)
 async def start_command(client: Client, message: Message):
     id = message.from_user.id
     if not await present_user(id):
