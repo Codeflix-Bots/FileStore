c80c5323928cd265e7b6eddb1ca5a460397d08e3
rohit3607 <rohitplayer87089@gmail.com>
2025-02-24 15:57:40 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,7 +8,6 @@ import sys
 import time
 import string
 import string as rohit
-import humanize
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
