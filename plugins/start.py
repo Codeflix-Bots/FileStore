c8c39db674e6916d9c9d9c8441bdc334f5f289cd
Mir Shakir <brainlessanime123@gmail.com>
2025-03-08 18:31:44 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -171,7 +171,7 @@ async def start_command(client: Client, message: Message):
 
     [
                     InlineKeyboardButton("𝗔𝗯𝗼𝘂𝘁", callback_data = "about"),
-                    InlineKeyboardButton('𝗖𝗵𝗮𝗻𝗻𝗲𝗹𝘀', url='https://t.me/nova_flix')
+                    InlineKeyboardButton('𝗖𝗵𝗮𝗻𝗻𝗲𝗹𝘀', url='https://t.me/cypherixsocity')
 
     ]
             ]
