989e74681489afa09eb35514f9008a1f69b6de1f
Mir Shakir <brainlessanime123@gmail.com>
2025-03-08 16:35:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -112,7 +112,7 @@ class Bot(Client):
 
         self.set_parse_mode(ParseMode.HTML)
         self.username = usr_bot_me.username
-        self.LOGGER(__name__).info(f"Bot Running..! Modify by @Cypherixsocity")   
+        self.LOGGER(__name__).info(f"Bot Running..! Made by @Codeflix_Bots")   
 
         # Start Web Server
         app = web.AppRunner(await web_server())
@@ -120,7 +120,7 @@ class Bot(Client):
         await web.TCPSite(app, "0.0.0.0", PORT).start()
 
 
-        try: await self.send_message(OWNER_ID, text = f"<b><blockquote>🤖 Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Cypherixsocity</blockquote></b>")
+        try: await self.send_message(OWNER_ID, text = f"<b><blockquote>🤖 Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Codeflix_Bots</blockquote></b>")
         except: pass
 
     async def stop(self, *args):
