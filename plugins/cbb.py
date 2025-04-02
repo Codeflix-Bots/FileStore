b46d51d2a8112e6033d6892f5af8ca19f02d6806
ʏᴀᴛᴏ <YatoSama064@gmail.com>
2025-04-02 19:15:32 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,3 +1,12 @@
+#
+# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
+#
+# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
+# and is released under the MIT License.
+# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
+#
+# All rights reserved.
+
 from pyrogram import Client 
 from bot import Bot
 from config import *
