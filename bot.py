4616787546060cc1b3fe525c02748cc29075ac43
rohit3607 <rohitplayer87089@gmail.com>
2025-05-01 17:15:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,16 @@
+
+# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
+# Ask Doubt on telegram @CodeflixSupport
+#
+# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
+#
+# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
+# and is released under the MIT License.
+# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
 from aiohttp import web
 from plugins import web_server
 import asyncio
@@ -5,15 +18,40 @@ import pyromod.listen
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 import sys
+import pytz
 from datetime import datetime
 #rohit_1888 on Tg
 from config import *
+from database.db_premium import *
+from database.database import *
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import logging
+
+# Suppress APScheduler logs below WARNING level
+logging.getLogger("apscheduler").setLevel(logging.WARNING)
+
+scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
+scheduler.add_job(remove_expired_users, "interval", seconds=10)
+
+# Reset verify count for all users daily at 00:00 IST
+async def daily_reset_task():
+    try:
+        await db.reset_all_verify_counts()
+    except Exception:
+        pass  
+
+scheduler.add_job(daily_reset_task, "cron", hour=0, minute=0)
+scheduler.start()
 
 
 name ="""
  BY CODEFLIX BOTS
 """
 
+def get_indian_time():
+    """Returns the current time in IST."""
+    ist = pytz.timezone("Asia/Kolkata")
+    return datetime.now(ist)
 
 class Bot(Client):
     def __init__(self):
@@ -42,12 +80,21 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeflixSupport for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/weebs_support for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeflixSupport")
-        self.LOGGER(__name__).info(f"""BOT DEPLOYED BY @CODEFLIX_BOTS""")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/weebs_support")
+        self.LOGGER(__name__).info(f"""       
+
+
+  ___ ___  ___  ___ ___ _    _____  _____  ___ _____ ___ 
+ / __/ _ \|   \| __| __| |  |_ _\ \/ / _ )/ _ \_   _/ __|
+| (_| (_) | |) | _|| _|| |__ | | >  <| _ \ (_) || | \__ \
+ \___\___/|___/|___|_| |____|___/_/\_\___/\___/ |_| |___/
+                                                         
+ 
+                                          """)
 
         self.set_parse_mode(ParseMode.HTML)
         self.username = usr_bot_me.username
